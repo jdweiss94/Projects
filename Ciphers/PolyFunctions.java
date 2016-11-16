3623cc1877034d99a4ddcd84f2c1e5afd65d7f0e
jdweiss94 <jdweiss94@gmail.com>
2016-11-16 17:06:12 -0500

    Completed PolyFunctions and PolyFunctionsTester

--- Ciphers/PolyFunctions.java
+++ Ciphers/PolyFunctions.java
@@ -1,4 +1,9 @@
-
+/**
+ * Justin Weiss
+ * 
+ * Utilizes polynomial functions mod 2
+ *
+ */
 public class PolyFunctions {
 	
 	public int add(int a, int b)
@@ -22,8 +27,78 @@ public class PolyFunctions {
 	}
 	public int reduce(int a, int b)
 	{
-		int x = 0;
-		return x;
+		if(getHighestBit(a) < getHighestBit(b))
+		{
+			return a;
+		}
+		else
+		{
+			int i = getHighestBitX(a);
+			int j = getHighestBitX(b);
+			int k = b << (i-j);
+			int temp = add(a,k);
+			return reduce(temp,b);
+		}
+	}
+	private int getHighestBit(int bit)
+	{
+		int left = 1;
+		int output = 0;
+		for(int i = 0; i < 30; i++)
+		{
+			if((left&bit)>0)
+			{
+				output = left;
+			}
+			left = left << 1;
+		}
+		return output;
+	}
+	private int getHighestBitX(int bit)
+	{
+		int left = 1;
+		int output = 0;
+		for(int i = 0; i < 30; i++)
+		{
+			if((left&bit)>0)
+			{
+				output = i;
+			}
+			left = left << 1;
+		}
+		return output;
+	}
+	public String matrices(int[][] a, int[][] b)
+	{
+		int[][] result = new int[4][4];
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				int temp1 = multiply(a[i][0],b[0][j]);
+				int temp2 = multiply(a[i][1],b[1][j]);
+				int temp3 = multiply(a[i][2],b[2][j]);
+				int temp4 = multiply(a[i][3],b[3][j]);
+				
+				temp1 = add(temp1,temp2);
+				temp3 = add(temp3,temp4);
+				temp1 = add(temp1,temp3);
+				
+				result[i][j] = temp1;
+			}
+		}
+		String tempS = "";
+		for(int i = 0; i < 4; i++)
+		{
+			tempS = tempS + "|";
+			for(int j = 0; j < 4; j++)
+			{
+				tempS = tempS + " " + result[i][j];
+			}
+			tempS = tempS + "|\n";
+		}
+		return tempS;
+		
 	}
 }
  
--- Ciphers/PolyFunctionsTester.java
+++ Ciphers/PolyFunctionsTester.java
@@ -1,4 +1,8 @@
-
+/**
+ * Justin Weiss
+ * 
+ * Tester class for PolyFunctions class. Expected results are noted above the called method
+ */
 public class PolyFunctionsTester {
 	public static void main(String args[])
 	{
@@ -27,5 +31,48 @@ public class PolyFunctionsTester {
 		 * Return should be 78
 		 */
 		System.out.println(poly.multiply(13,6));
+		/**
+		 * 7 = 111 = x^2+x+1
+		 * 5  = 101 = x^2+1
+		 * 			  ______1
+		 * 		x^2+1|x^2+x+1
+		 * 			  x^2+  1
+		 * 			   	  x
+		 * 
+		 * x = 10 = 2
+		 * Return should be 2
+		 */
+		System.out.println(poly.reduce(7,5));
+		/**
+		 * a =  |1 0 1 1|
+		 * 		|1 0 1 0|
+		 * 		|1 0 1 0|
+		 * 		|1 0 1 1|
+		 * 
+		 * b =  |1 1 0 1|
+		 * 		|1 0 1 1|
+		 * 		|1 1 1 0|
+		 * 		|1 0 0 1|
+		 * 
+		 * The returned result should be |1 0 1 0|
+		 * 								 |0 0 1 1|
+		 * 								 |0 0 1 1|
+		 * 								 |1 0 1 0|
+		 * 
+		 */
+		int[][] a = new int[][]{
+			{1,0,1,1},
+			{1,0,1,0},
+			{1,0,1,0},
+			{1,0,1,1}
+		};
+		int[][] b = new int[][]{
+			{1,1,0,1},
+			{1,0,1,1},
+			{1,1,1,0},
+			{1,0,0,1}
+		};
+		
+		System.out.println(poly.matrices(a, b));
 	}
 }
