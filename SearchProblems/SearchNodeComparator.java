a16395f974a6eb21aab91aaad6a0e6fb8e923be4
jdweiss94 <jdweiss94>
2017-02-24 18:17:50 -0500

    Added comparator class

---
+++ SearchProblems/SearchNodeComparator.java
@@ -0,0 +1,26 @@
+import java.util.Comparator;
+
+
+public class SearchNodeComparator implements Comparator<SearchNode> 
+{	
+
+	public int compare(SearchNode node1, SearchNode node2) 
+	{
+		if(node1.getG() < node2.getG())
+		{
+			return 1;
+		}
+		else if(node1.getG() > node2.getG())
+		{
+			return -1;
+		}
+		
+		else
+		{
+			return 0;
+		}
+	
+	}
+
+}
+	
